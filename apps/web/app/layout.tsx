dc7a24020261b159a2716b7039fd1b988bf377a5
btomas <pasantesistemas2@argental.com.ar>
2026-05-28 07:39:04 -0300

    Fix al cargar pagina

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -10,10 +10,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <head>
-        <link rel="preload" href="/busquetti/BusquettiEsperandoV5.mp4" as="video" type="video/mp4" />
-        <link rel="preload" href="/busquetti/BusquettiHablandoV5.mp4" as="video" type="video/mp4" />
-      </head>
       <body>{children}</body>
     </html>
   );
